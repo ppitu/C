cbbcd513bbf7b7fb21955757d2d1ba2bbab8b75c
Piotrek <piotr.chmura.1998@o2.pl>
2019-09-11 20:19:37 +0200

    First Commit

---
+++ Wydatki/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "menu.h"
+
+void wypiszMiesiac();
+
+int main()
+{
+	wyswietlMenu();
+	wypiszMiesiac();	
+	return 0;
+}
+
+void wypiszMiesiac()
+{
+	printf("Styczen 2019\n");
+	printf("Dom\tSamochod\tNauka\tRozrywka\tJedzenie\tUbrania\tInne\n");
+	
+}
---
+++ Wydatki/menu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "menu.h"
+
+void wyswietlMenu()
+{
+	printf("Wyswietl aktualny miesiac\n");
+
+}
