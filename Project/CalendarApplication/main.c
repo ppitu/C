c2593ec6ad63a1dd345cb5bb439ea96e708f7076
Piotrek <piotr.chmura.1998@o2.pl>
2019-10-13 17:37:36 +0200

    add

--- Project/CalendarApplication/main.c
+++ Project/CalendarApplication/main.c
@@ -1,70 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-
-int leap(int year)
-{
-	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
-}
-
-int numberOfDay(int month, int year)
-{
-	int tab[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	int tab1[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
-	if(leap(year))
-		return tab1[month];
-	else 
-		return tab[month];
-}
-
-int dayOfWeek(int m, int y)
-{
+int zeller(int m, int y){
 	int d = 1;
-	int Y,C,M,N,D;
-
+	int Y, C, M, N, D;
 	M = 1 + (9 + m) % 12;
-	Y = y - (M > 10);
+	Y = y - ( M > 10);
 	C = Y / 100;
 	D = Y % 100;
-
-	N =((13 * M - 1) / 5 + D + D / 4 + C / 4 + 5 * C + d) % 7;
-
+	N = ((13 * M - 1) / 5 + D + D / 4 + C / 4 + 5 * C +d) % 7;
 	return (7 + N) % 7;
 }
 
-void printCalendar(int month, int year)
+void displayMonth(int month, int year)
 {
-	puts("Sun\tMon\tTue\tWed\tThu\tFri\tSat");
+	int dayInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-	int x = dayOfWeek(month, year);
-
-	int numberofday = numberOfDay((month - 1), year);
-
-	for(size_t i = 0; i < x; i++)
-		printf("\t");
-
-	for(int i = 1; i <= numberofday; i++)
+	int startday = zeller(month, year);
+	int day = 1;
+	
+	puts("Sun\tMon\tTue\tWen\tThu\tFri\tSat");
+	for(int i = 0; i < dayInMonth[month - 1]; i++)
 	{
-		printf("%i\t", i);
-			
-		x++;
+		if(startday > i)
+		{
+			printf("\t");
+			continue;
+		}
 
-		if((x % 7) == 0)
+		if((i % 7) == 0)
 			printf("\n");
+		printf("%d\t", day);
+		day++;
 	}
-	printf("\n");
-
 }
 
 int main()
 {
-	system("clear");
-	int month, year;
-	printf("Enter the month: ");
-	scanf("%i", &month);
-	printf("Enter the year: ");
-	scanf("%i", &year);
-
-	printCalendar(month, year);
+	displayMonth(10, 2019);
 }
